a4f4f42accdb22eb18e082f2e059017f65657773
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-11 21:14:35 +0600

    implementing the methods of 'MyHashTableInterface'

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,2 +1,27 @@
-public class MyHashTable<K, V> {
+public class MyHashTable<K, V> implements MyHashTableInterface<K, V>{
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(V value) {
+        return false;
+    }
+
+    @Override
+    public K getKey(V value) {
+        return null;
+    }
 }
