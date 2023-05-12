ba287e73f933e99398a6e04abf40552e0d08526d
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-12 15:21:44 +0600

    declaring the constructor for 'MyTestingClass'

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,4 +1,9 @@
 public class MyTestingClass {
     private String name;
     private int id;
+
+    public MyTestingClass(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 }
