a6d9f5743dbf3e8668f3033863e78b49842fb78f
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-14 17:55:09 +0600

    updating 'main'

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
     private static final String[] standNames = {"Star Platinum", "The World", "Crazy Diamond", "Killer Queen",
             "Gold Experience", "King Crimson", "Sticky Fingers", "Tusk", "D4C", "Made in Heaven"};
     private static final int NUM_STANDS = standNames.length;
-
     private static final Random rand = new Random();
 
     public static void main(String[] args) {
@@ -19,7 +18,6 @@ public class Main {
             String standName = standNames[rand.nextInt(NUM_STANDS)];
 
             MyTestingClass key = new MyTestingClass(standName + i, rand.nextInt(1000));
-
             StandUser value = new StandUser("name" + i, rand.nextInt(20) + 10);
 
             hashTable.put(key, value);
