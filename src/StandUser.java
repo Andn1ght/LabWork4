068cfb91ea90e084210c8560d09794af9e858c12
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-14 17:30:57 +0600

    implementing 'equals' method for 'StandUser' class

--- src/StandUser.java
+++ src/StandUser.java
@@ -14,4 +14,16 @@ public class StandUser {
     public int getAge() {
         return age;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof StandUser)) {
+            return false;
+        }
+        StandUser other = (StandUser) obj;
+        return this.name.equals(other.name) && this.age == other.age;
+    }
 }
